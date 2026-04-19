b7f1676feda1f9e38fdc12312db2d535fea18fc9
Slazanger <steve@shockmonkey.net>
2026-04-19 20:02:03 +0100

    removed old inspector

--- SST/ViewModels/MainWindowViewModel.cs
+++ SST/ViewModels/MainWindowViewModel.cs
@@ -8,15 +8,12 @@ public partial class MainWindowViewModel : ViewModelBase
     public MainWindowViewModel()
     {
         Sync = new SyncViewModel();
-        Inspector = new InspectorViewModel();
     }
 
     public SyncViewModel Sync { get; }
-    public InspectorViewModel Inspector { get; }
 
     public void AttachShell(Window shell)
     {
         Sync.AttachShell(shell);
-        Inspector.AttachShell(shell);
     }
 }
